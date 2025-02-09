12a92fd89ea60dc68ae492262f6056ab3d38994f
Daniel Eduardo Useche <du.important@gmail.com>
2025-02-08 18:20:33 -0800

    Actual

---
+++ LAB 01/shapes/shapes/__SHELL70.java
@@ -0,0 +1,14 @@
+
+public class __SHELL70 extends bluej.runtime.Shell {
+public static java.lang.Object run() throws Throwable {
+final bluej.runtime.BJMap __bluej_runtime_scope = getScope("C:\\Users\\tivic\\OneDrive\\Documentos\\Clone\\POOB\\LAB 01\\shapes\\shapes");
+final KalahGame kalahGam1 = (KalahGame)__bluej_runtime_scope.get("kalahGam1");
+
+
+int __bluej_param0 = 1;
+try {
+return makeObj(kalahGam1.makeMove(__bluej_param0)
+);}
+finally {
+}
+}}

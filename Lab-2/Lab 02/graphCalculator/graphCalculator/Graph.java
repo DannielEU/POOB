5559dabb35612bcd54153ff95598353563a358df
DannielEU <du.important@gmail.com>
2025-02-13 16:54:50 -0500

    Lab-2
    Inicio del Laboratorio 2

---
+++ Lab-2/Lab 02/graphCalculator/graphCalculator/Graph.java
@@ -0,0 +1,47 @@
+    import java.util.ArrayList;
+
+public class Graph {
+    
+    
+    public Graph(String[] vertices, String[][] edges){
+    }
+    
+
+    public boolean contains(String vertex){
+        return false;
+    }
+    
+    
+    public Graph path(String start, String end){
+        return null;
+    }
+
+    public Graph union (Graph g){
+        return null;
+    }
+
+    
+    public int vertices(){
+        return 0;
+    }
+    
+   
+    public int edges(){
+        return 0;
+    }    
+    
+    
+    public boolean equals(Graph g){
+        return false;
+    }
+    
+    public boolean equals(Object g){
+        return equals((Graph)g);
+    }
+    
+    //Only arcs in space-separated tuples. The vertices are capitalized. The edges must always be in alphabetical order.
+    //For example, (A, B) (C, D) 
+    public String toString() {
+      return "";
+    }
+}
